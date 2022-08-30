24a7fbff44eb0ea54d36a146c001103138ac5723
Sanchayan Bhunia <sanchayan.bhunia@hotmail.com>
2022-08-30 15:58:19 +0200

    Dendritic cells, TCells and BCells Added

--- src/universe/agents/DendriticCellAgent.java
+++ src/universe/agents/DendriticCellAgent.java
@@ -1,5 +1,32 @@
 package universe.agents;
 
-public class DendriticCellAgent {
+import jade.core.Agent;
+import jade.core.behaviours.OneShotBehaviour;
+
+public class DendriticCellAgent extends Agent{
         
+    private int[] viralCodon;
+    
+    public void setViralCodon(int[] codon) {
+        this.viralCodon = codon;
+    }
+
+
+    /* Setup Method */
+
+    @Override
+    protected void setup() {
+        /* Behaviours of Dendritic Cell */
+    }
+
+    private class StartSearchingForVirus extends OneShotBehaviour {
+
+        @Override
+        public void action() {
+            
+            
+        }
+
+    }
+
 }
